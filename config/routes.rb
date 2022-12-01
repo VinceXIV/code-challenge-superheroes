af7ba9803b2cc093de9bbc1ce08f371b4dbb2475
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-01 19:59:58 +0300

    Implement the required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :hero_powers, only: [:create]
+  resources :hero_powers, only: [:index, :create]
   resources :powers, only: [:index, :show, :update]
-  resources :heros, only: [:index, :show]
+  # resources :heros, only: [:index, :show]
+
+  get '/heroes', to: 'heros#index'
+  get '/heroes/:id', to: 'heros#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
